cf4a07adcd46c640ad62ce4856b4d9cacbf13916
minhajul-karim <minhajul.kaarim@gmail.com>
2021-10-02 12:20:22 +0600

    Initialize dashboard template from MUI

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
-import ProTip from './ProTip';
+import ProTip from './components/ProTip';
+import Dashboard from './components/Dashboard';
 
 function Copyright() {
   return (
@@ -19,14 +20,6 @@ function Copyright() {
 
 export default function App() {
   return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Create React App v5 example with styled-components
-        </Typography>
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
+    <Dashboard />
   );
 }

4bd465d28380101ff05bfadb9f3bbd396e3a5521
minhajul-karim <minhajul.kaarim@gmail.com>
2021-10-28 19:19:13 +0600

    Deploy: Add _redirects to client and disable eslint errors

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
 import Axios from 'axios';
 import { Home } from './pages';
 
-Axios.defaults.baseURL = 'http://localhost:5000/api/';
+// eslint-disable-next-line no-unused-vars
+const localApi = 'http://localhost:5000/api/';
+const remoteApi = 'https://gooods.herokuapp.com/api/';
+
+Axios.defaults.baseURL = remoteApi;
 
 export default function App() {
   return <Home />;
--- client/src/components/enhancedTable/index.js
+++ client/src/components/enhancedTable/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/no-unused-prop-types */
 import {
   Button,

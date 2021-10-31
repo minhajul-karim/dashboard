04d92ba4dbb7abef0951bc7a6e9482e58b5e3668
minhajul-karim <minhajul.kaarim@gmail.com>
2021-10-31 15:01:35 +0600

    aa

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,7 @@ import Axios from 'axios';
 import { Home } from './pages';
 
 Axios.defaults.baseURL =
-  // eslint-disable-next-line no-constant-condition
-  process.env.NODE_ENV === 'development' || 'test'
+  process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test'
     ? process.env.REACT_APP_DEV_API_URL
     : process.env.REACT_APP_PROD_API_URL;
 
